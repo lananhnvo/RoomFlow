33de5901aab7762f2a76aa5b0c7f5fb796869e52
ssejica <jessicaguo997@gmail.com>
2024-03-30 17:36:46 -0400

    availability

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('hotelChain').addEventListener('change', function() {
         var areas = {
-            'Celestia': ['Lanxia', 'Dreamscape Delta', 'Mystic Mountain', 'Enchatment Valley', 'Elysium Estate'],
+            'Celestia': ['Lanxia', 'Dreamscape Delta', 'Mystic Mountain', 'Enchantment Valley', 'Elysium Estate'],
             'Emporia': ['Tower of Hope', 'Eternal Gardens', 'Monument of Peace', 'Liberty Square'],
-            'Jade Empire': ['Archive of Ages', 'Palace of Peace', 'Garden of Harmony', 'Fortress of Fortitude']
-        };
+            'Jade Empire': ['Archive of Ages', 'Palace of Peace', 'Garden of Harmony', 'Fortress of Fortitude'],
+            'Lotus Luxe': ['Palace of Peace', 'Fortress of Fortitude', 'Temple of Tranquility', 'Garden of Harmony'],
+            'Mystica': ['Realm of Bliss', 'District of Delight', 'Sector of Serenity', 'Province of Pleasure']
+        };        
 
         var selectedChain = this.value;
         var areaSelect = document.getElementById('area');
