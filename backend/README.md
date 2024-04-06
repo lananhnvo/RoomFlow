947414c7e52127ed42f20061bc45e96ea20038cd
ssejica <jessicaguo997@gmail.com>
2024-04-05 23:14:56 -0400

    tried

--- backend/README.md
+++ backend/README.md
@@ -3,3 +3,7 @@
 `py -m venv .venv`
 
 `.venv\Scripts\activate && py -m pip install --upgrade pip && pip install -r requirements.txt`
+
+# Running the Backend
+
+`flask --app python run`
--- backend/python.py
+++ backend/python.py
@@ -1,24 +1,32 @@
+from flask import Flask, jsonify
 import psycopg2
-
-# Connect to your postgres DB
-conn = psycopg2.connect("dbname=postgres user=postgres")
-
-# Open a cursor to perform database operations
-cur = conn.cursor()
-
-from flask import Flask
+import psycopg2.extras
 
 app = Flask(__name__)
 
-@app.route("/search")
-def search():
-    return "<p>Hello, World!</p>"
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname='RoomFlow', 
+        user='postgres', 
+        password='2409', 
+        host='localhost'
+    )
+    return conn
 
-@app.route("/edit")
-def edit():
-    return <p>
+@app.route('/api/rooms')
+def get_rooms():
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute('''
+        SELECT r.room_number, h.address, hc.name as hotel_chain_name, r.price, r.capacity
+        FROM room r
+        JOIN hotel h ON r.hotel_id = h.id
+        JOIN hotel_chain hc ON h.hotel_chain_id = hc.id
+    ''')
+    rooms = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(rooms)
 
-@app.route("/selectall")
-def selectall():
-    cur.execute("SELECT * FROM my_data")
-    return cur.fetchall()
+if __name__ == '__main__':
+    app.run(debug=True)
